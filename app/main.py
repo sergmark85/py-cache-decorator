e1727fb673f2372688d7f59077a0d94a482233d5
Serhii Markusyk <serega.markusik@gmail.com>
2025-05-11 14:00:32 +0200

    cache decorator

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
-from typing import Callable
-
+from typing import Callable, Any
+from functools import wraps
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    dict_result = {}
+
+    @wraps(func)
+    def inner(*args: Any, **kwargs: Any) -> Any:
+        key = (args, tuple(kwargs.items()))
+        if key in dict_result:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            dict_result[key] = func(*args, **kwargs)
+        return dict_result[key]
+    return inner
+
